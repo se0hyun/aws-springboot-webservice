650f4efbb65122eb530effd9a8eabb9c968a9444
se0hyun <kimlisa00@seoultech.ac.kr>
2024-02-07 15:12:34 +0900

    Initial commit

---
+++ src/main/java/com/springbootaws/book/awsspringbootwebservice/AwsSpringbootWebserviceApplication.java
@@ -0,0 +1,14 @@
+package com.springbootaws.book.awsspringbootwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsSpringbootWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsSpringbootWebserviceApplication.class, args);
+	}
+
+}
+ 
---
+++ src/test/java/com/springbootaws/book/awsspringbootwebservice/AwsSpringbootWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootaws.book.awsspringbootwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsSpringbootWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
