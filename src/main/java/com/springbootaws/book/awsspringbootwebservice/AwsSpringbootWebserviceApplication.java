e8ca0c40a89ffd3c32a4fec401e0d4e906612152
se0hyun <kimlisa00@seoultech.ac.kr>
2024-02-07 17:06:29 +0900

    DOCS main 실행 주석 추가

--- src/main/java/com/springbootaws/book/awsspringbootwebservice/AwsSpringbootWebserviceApplication.java
+++ src/main/java/com/springbootaws/book/awsspringbootwebservice/AwsSpringbootWebserviceApplication.java
@@ -3,12 +3,12 @@ package com.springbootaws.book.awsspringbootwebservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class AwsSpringbootWebserviceApplication {
+
+@SpringBootApplication	// 해당 애노테이션으로 스프링 부트의 자동 설정, 스프링 빈 읽기와 생성이 모두 자동으로 설정됨
+public class AwsSpringbootWebserviceApplication {	// main class
 
 	public static void main(String[] args) {
 		SpringApplication.run(AwsSpringbootWebserviceApplication.class, args);
-	}
+	}	// 내장 was 실행 -> 서버에 톰캣을 설치할 필요가 없고, JAR 파일로 실행 가능
 
 }
- 
